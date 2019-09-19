566c4894af5fbce90d117d31158c0bbddb62c2d3
lassegh <lasse.groenbech@gmail.com>
2019-09-19 09:45:48 +0200

    Initial commit

---
+++ BookLibrary/Book.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace BookLibrary
+{
+    public class Book
+    {
+        private string _title;
+        private int _numberOfPages;
+        private string _iSBN;
+
+        public Book()
+        {
+            
+        }
+
+        public Book(string title, int numberOfPages, string iSbn, string author)
+        {
+            Title = title;
+            NumberOfPages = numberOfPages;
+            ISbn = iSbn;
+            Author = author;
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (value.Length < 2)
+                {
+                    throw new ArgumentOutOfRangeException("Title cannot be less than 2 characters long");
+                }
+                _title = value;
+            }
+        }
+
+        public string Author { get; set; }
+
+        public int NumberOfPages
+        {
+            get { return _numberOfPages; }
+            set
+            {
+                if (value >= 10 && value <= 1000)
+                {
+                   _numberOfPages = value; 
+                }
+                else throw new ArgumentOutOfRangeException("Number of pages must be more than 9 and less than 1001");
+            }
+        }
+
+        public string ISbn
+        {
+            get { return _iSBN; }
+            set
+            {
+                if (value.Length != 13)
+                {
+                    throw new ArgumentOutOfRangeException("ISBN must be 13 characters long");
+                }
+                _iSBN = value;
+            }
+        }
+    }
+}
---
+++ BookLibraryTest/BookTest.cs
@@ -0,0 +1,97 @@
+using System;
+using BookLibrary;
+using Microsoft.VisualStudio.TestPlatform.Common.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BookLibraryTest
+{
+    /// <summary>
+    /// Test af bogklassen i BookLibrary
+    /// </summary>
+    [TestClass]
+    public class BookTest
+    {
+        Book book;
+
+        [TestInitialize]
+        public void Init()
+        {
+            book = new Book();
+        }
+
+        [TestMethod]
+        public void TestTitlePass() // Tester en titel med 2 chars - dette er minimumskravet
+        {
+            book.Title = "Aa";
+            Assert.AreEqual("Aa",book.Title);
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void TestTitleFail() // Tester titel med 1 char - dette er lige på den forkerte side af det tilladte
+        {
+            book.Title = "A";
+        }
+
+        [TestMethod]
+        public void TestAuthor() // Tester getter og setter på Author
+        {
+            book.Author = "LG";
+            Assert.AreEqual("LG",book.Author);
+        }
+
+        [TestMethod]
+        public void TestPagesPass() // Tester de to tilladte yderpunkter på antal af sider
+        {
+            book.NumberOfPages = 10;
+            Assert.AreEqual(10,book.NumberOfPages);
+            book.NumberOfPages = 1000;
+            Assert.AreEqual(1000,book.NumberOfPages);
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void TestPagesFailOne() // Tester at der kommer exception på den ene forkerte side af det tilladte antal sider
+        {
+            book.NumberOfPages = 9;
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void TestPagesFailTwo() // Tester at der kommer exception på den anden forkerte side af det tilladte antal sider
+        {
+            book.NumberOfPages = 1001;
+        }
+
+        [TestMethod]
+        public void TestIsbnPass() // Tester ISBN med de tilladte 13 chars
+        {
+            book.ISbn = "0123456789123";
+            Assert.AreEqual("0123456789123",book.ISbn);
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void TestIsbnFailOne() // Tester ISBN med 14 chars
+        {
+            book.ISbn = "01234567891234";
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void TestIsbnFailTwo() // Tester ISBN med 12 chars
+        {
+            book.ISbn = "012345678912";
+        }
+
+        [TestMethod]
+        public void TestConstructor() // Tester parameterfyldt konstruktør
+        {
+            Book myBook = new Book("Aa",10,"0123456789123","LG");
+            Assert.AreEqual(10,myBook.NumberOfPages);
+            Assert.AreEqual("Aa",myBook.Title);
+            Assert.AreEqual("0123456789123",myBook.ISbn);
+            Assert.AreEqual("LG",myBook.Author);
+        }
+    }
+}
